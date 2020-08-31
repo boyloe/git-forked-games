a6b71bb19f8ac28b0fac86edf7222fb7fefda561
DamonLC21 <chiversdamon@gmail.com>
2020-08-31 10:44:43 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Game.create(name: "Rainbow Six Siege", image_url: "https://store.ubi.com/dw/image/v2/ABBS_PRD/on/demandware.static/-/Sites-masterCatalog/en_US/dw44a8a154/images/large/5e159fce5cdf9a1ec45ad825.jpg") 
+Game.create(name: "Super Mario Galaxy", image_url: "https://s1.gaming-cdn.com/images/products/6723/orig/super-mario-galaxy-switch-cover.jpg")
+Game.create(name: "The Last of Us II", image_url: "https://images-na.ssl-images-amazon.com/images/I/71p8G%2BYeA6L._SL1500_.jpg")
